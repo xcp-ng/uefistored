aca067668f7e23cdbb08868a775903066e419730
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-05-01 15:27:02 -0500

    Add attrs to db_get/_set

--- inc/backends/mem.h
+++ inc/backends/mem.h
@@ -1,9 +1,11 @@
 #ifndef __H_VARSTOREDMEM_
 #define __H_VARSTOREDMEM_
 
+#include <stdint.h>
+
 int db_init(void);
 void db_deinit(void);
-int db_get(void *, size_t, void** , size_t *);
-int db_set(void *, size_t, void *, size_t);
+int db_get(void *, size_t, void** , size_t *, uint32_t*);
+int db_set(void *, size_t, void *, size_t, uint32_t);
 
 #endif
--- src/backends/mem.c
+++ src/backends/mem.c
@@ -21,24 +21,44 @@
 
 #define DBPATH "/var/run/xen/varstored-db.dat"
 #define DBPATH_VAR_LEN "/var/run/xen/varstored-db-var-len.dat"
-#define ENTRY_LEN 1024
+#define DBPATH_VAR_ATTRS "/var/run/xen/varstored-db-var-attrs.dat"
 
+#define ENTRY_LEN 1024
 #define KISSDB_KEY_SIZE 128 
 #define KISSDB_VAL_SIZE 1024
+#define KISSDB_VAR_ATTRS_VAL_SIZE (sizeof(uint32_t))
 
 static KISSDB db;
 static KISSDB db_var_len;
+static KISSDB db_var_attrs;
 
 int db_init(void)
 {
     int ret;
     ret = KISSDB_open(&db, DBPATH, KISSDB_OPEN_MODE_RWCREAT, 1024, KISSDB_KEY_SIZE, KISSDB_VAL_SIZE);
-
     if ( ret < 0 )
-        return ret;
+        return -1;
 
-    return  KISSDB_open(&db_var_len, DBPATH_VAR_LEN, KISSDB_OPEN_MODE_RWCREAT, 1024, KISSDB_KEY_SIZE, sizeof(size_t));
+    ret = KISSDB_open(&db_var_len, DBPATH_VAR_LEN, KISSDB_OPEN_MODE_RWCREAT,
+                      1024, KISSDB_KEY_SIZE, sizeof(size_t));
+    if ( ret < 0 )
+        goto close1;
+
+    ret = KISSDB_open(&db_var_attrs, DBPATH_VAR_LEN, KISSDB_OPEN_MODE_RWCREAT,
+                      1024, KISSDB_KEY_SIZE, KISSDB_VAR_ATTRS_VAL_SIZE);
+    if ( ret < 0 )
+        goto close2;
+
+    return 0;
+
+close2:
+    KISSDB_close(&db_var_len);
+
+close1:
+    KISSDB_close(&db);
+    return ret;
 }
+
 
 void db_deinit(void)
 {
@@ -46,8 +66,9 @@ void db_deinit(void)
     KISSDB_close(&db_var_len);
 }
 
-int db_get(void *varname, size_t varname_len, void** dest, size_t *len)
+int db_get(void *varname, size_t varname_len, void** dest, size_t *len, uint32_t *attrs)
 {
+    int ret;
     uint8_t key[KISSDB_KEY_SIZE] = {0};
     uint8_t val[KISSDB_VAL_SIZE] = {0};
     size_t tmp;
@@ -61,10 +82,28 @@ int db_get(void *varname, size_t varname_len, void** dest, size_t *len)
     memcpy(&key, varname, varname_len);
 
     /* Get the variable's value */
-    KISSDB_get(&db, key, &val);
+    ret = KISSDB_get(&db, key, &val);
+    if ( ret < 0 )
+    {
+        ERROR("Missing in var db\n");
+        return ret;
+    }
 
     /* Get the variable's value's length */
-    KISSDB_get(&db_var_len, key, &tmp);
+    ret = KISSDB_get(&db_var_len, key, &tmp);
+    if ( ret < 0 )
+    {
+        ERROR("Missing in var len db\n");
+        return ret;
+    }
+
+    /* Get the variable's attrs */
+    ret = KISSDB_get(&db_var_attrs, key, attrs);
+    if ( ret < 0 )
+    {
+        ERROR("Missing in var attrs db\n");
+        return ret;
+    }
 
     /* Copy only the correct length (from db_var_len) */
     *dest = malloc(tmp);
@@ -74,7 +113,7 @@ int db_get(void *varname, size_t varname_len, void** dest, size_t *len)
     return 0;
 }
 
-int db_set(void *varname, size_t varlen, void *val, size_t len)
+int db_set(void *varname, size_t varlen, void *val, size_t len, uint32_t attrs)
 {
     uint8_t key[KISSDB_KEY_SIZE] = {0};
     uint8_t padval[KISSDB_VAL_SIZE] = {0};
@@ -108,6 +147,11 @@ int db_set(void *varname, size_t varlen, void *val, size_t len)
     ret = KISSDB_put(&db_var_len, key, &len);
     if ( ret < 0 )
         return ret;
+
+    ret = KISSDB_put(&db_var_attrs, key, &attrs);
+    if ( ret < 0 )
+        return ret;
+
 
     return 0;
 }
