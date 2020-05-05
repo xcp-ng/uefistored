cecc42d6fb4e9b034b02b0b33d0b81d24bf9786f
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-05-04 19:24:41 -0500

    Add kissdb to Makefile fetch target

--- Env.mk
+++ Env.mk
@@ -1,5 +1,5 @@
 # GIT repo dependencies
-GIT_REPO_TARGETS :=  xcp-ng-build-env/.cloned
+GIT_REPO_TARGETS :=  xcp-ng-build-env/.cloned libs/kissdb/.cloned
 
 XCP_NG_VER := 8.1
 DOCKER_IMAGE := xcp-ng/xcp-ng-build-env:$(XCP_NG_VER)
@@ -9,6 +9,7 @@ DOCKER_ARGS := -v$(PWD):$(PWD) -w $(PWD) -it $(DOCKER_IMAGE)
 fetch: $(GIT_REPO_TARGETS)
 	@echo "$@ done!"
 
+
 .PHONY: docker-build
 docker-build: xcp-ng-build-env/.cloned
 	cd xcp-ng-build-env && ./build.sh $(XCP_NG_VER)
@@ -16,8 +17,8 @@ docker-build: xcp-ng-build-env/.cloned
 print-%:
 	@:$(info $($*))
 
-# Generic GIT repo clones
 xcp-ng-build-env/.cloned: GIT_REPO := https://github.com/beshleman/xcp-ng-build-env.git
+libs/kissdb/.cloned: GIT_REPO := git@github.com:adamierymenko/kissdb.git libs/kissdb
 
 %/.cloned:
 	git clone $(GIT_REPO)
