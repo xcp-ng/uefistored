bac9e5fcd9885f77edbd48e50d60b76d5b0ec2cc
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-10-06 14:47:14 -0700

    variables_service: move evaluate_attrs and set_efi_runtime to xen_variable_server.c
    Signed-off-by: Bobby Eshleman <bobbyeshleman@gmail.com>

--- inc/variables_service.h
+++ inc/variables_service.h
@@ -1,12 +1,5 @@
 #ifndef __H_VARIABLES_SERVICE__
 #define __H_VARIABLES_SERVICE__
 
-#include <stdint.h>
-
-#include "uefi/types.h"
-
-/* Public for unit tests */
-EFI_STATUS evaluate_attrs(uint32_t attrs);
-void set_efi_runtime(bool runtime);
 
 #endif // __H_VARIABLES_SERVICE__
--- src/variables_service.c
+++ src/variables_service.c
@@ -7,32 +7,3 @@
 #include "varnames.h"
 #include "variable.h"
 
-#define DEBUG_VARIABLES_SERVICE 1
-
-bool efi_at_runtime = false;
-
-void set_efi_runtime(bool runtime)
-{
-    efi_at_runtime = runtime;
-}
-
-EFI_STATUS evaluate_attrs(uint32_t attrs)
-{
-    /* No support for hardware error record */
-    if (attrs & EFI_VARIABLE_HARDWARE_ERROR_RECORD)
-        return EFI_UNSUPPORTED;
-    /* If RT is set, BS must also be set */
-    else if ((attrs & RT_BS_ATTRS) == EFI_VARIABLE_RUNTIME_ACCESS)
-        return EFI_INVALID_PARAMETER;
-    /* Not both authentication bits may be set */
-    else if ((attrs & EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS) && \
-             (attrs & EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS))
-        return EFI_SECURITY_VIOLATION;
-    /* We do not support EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS */
-    else if (attrs & EFI_VARIABLE_AUTHENTICATED_WRITE_ACCESS)
-        return EFI_SECURITY_VIOLATION;
-    else if (attrs & EFI_VARIABLE_APPEND_WRITE)
-        DDEBUG("attrs & EFI_VARIABLE_APPEND_WRITE");
-
-    return EFI_SUCCESS;
-}
