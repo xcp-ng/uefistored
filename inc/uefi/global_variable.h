9c1888f9990b0501bf3e95feceaaaa2eb0b15824
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-10-13 13:41:57 -0700

    Organize global var guid definitions
    Signed-off-by: Bobby Eshleman <bobbyeshleman@gmail.com>

--- inc/uefi/global_variable.h
+++ inc/uefi/global_variable.h
@@ -17,6 +17,8 @@
 #ifndef __GLOBAL_VARIABLE_GUID_H__
 #define __GLOBAL_VARIABLE_GUID_H__
 
+#include "uefi/types.h"
+
 #define EFI_GLOBAL_VARIABLE \
   { \
     0x8BE4DF61, 0x93CA, 0x11d2, {0xAA, 0x0D, 0x00, 0xE0, 0x98, 0x03, 0x2B, 0x8C } \
