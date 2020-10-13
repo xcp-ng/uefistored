aaef9a585966392f7542698e39f7b5a5d4f2e107
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-10-13 13:40:35 -0700

    Change ProcessVarWithPk to process_var_with_pk
    Signed-off-by: Bobby Eshleman <bobbyeshleman@gmail.com>

--- inc/uefi/auth.h
+++ inc/uefi/auth.h
@@ -14,4 +14,7 @@ CleanCertsFromDb(void);
 EFI_STATUS auth_internal_find_variable(UTF16 *name, EFI_GUID *guid,
                                            void **data, uint64_t *data_size);
 
+EFI_STATUS process_var_with_pk(UTF16 *name, EFI_GUID *guid, void *data,
+                            uint64_t data_size, uint32_t attrs, bool IsPk);
+
 #endif // __H__AUTH_
