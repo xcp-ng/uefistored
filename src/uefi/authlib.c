b1f9d0a29ffe0f70f452dd5d2dc99021ba34b565
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-11-20 10:55:54 -0800

    refactor(uefi/authlib.c): fix style

--- src/uefi/authlib.c
+++ src/uefi/authlib.c
@@ -40,12 +40,10 @@ extern bool secure_boot_enabled;
 
 uint8_t setup_mode;
 
-static EFI_GUID signature_support[] = {
-    EFI_CERT_SHA1_GUID,
-    EFI_CERT_SHA256_GUID,
-    EFI_CERT_RSA2048_GUID,
-    EFI_CERT_X509_GUID
-};
+static EFI_GUID signature_support[] = { EFI_CERT_SHA1_GUID,
+                                        EFI_CERT_SHA256_GUID,
+                                        EFI_CERT_RSA2048_GUID,
+                                        EFI_CERT_X509_GUID };
 
 /*
  * Hash context pointer
@@ -95,7 +93,7 @@ int auth_lib_load(struct auth_data *auths, size_t n)
         return -1;
 
     ret = 0;
-    for (i=0; i<n; i++) {
+    for (i = 0; i < n; i++) {
         if (load_auth(&auths[i]) < 0) {
             ret = -1;
             ERROR("error opening file %s\n", auths[i].path);
@@ -113,19 +111,17 @@ EFI_STATUS load_auth_files(struct auth_data *auths, size_t n)
     EFI_STATUS status;
     variable_t *var;
 
-    for (i=0; i<n; i++) {
+    for (i = 0; i < n; i++) {
         var = &auths[i].var;
 
         if (!storage_exists(var->name, var->namesz, &var->guid)) {
-            status = auth_lib_process_variable(
-                        var->name, var->namesz, &var->guid,
-                        var->data, var->datasz,
-                        var->attrs);
+            status = auth_lib_process_variable(var->name, var->namesz,
+                                               &var->guid, var->data,
+                                               var->datasz, var->attrs);
 
             if (status != EFI_SUCCESS) {
                 DDEBUG("Failed to set SB variable from %s, status=%s (0x%02lx)\n",
-                        auths[i].path,
-                        efi_status_str(status), status);
+                       auths[i].path, efi_status_str(status), status);
             }
         }
     }
@@ -149,48 +145,50 @@ auth_lib_initialize(struct auth_data *auths, size_t n)
 
     hash_ctx = malloc(sizeof(SHA256_CTX));
 
-    if (!storage_exists(L"PK", sizeof_wchar(L"PK"), &gEfiGlobalVariableGuid))  {
+    if (!storage_exists(L"PK", sizeof_wchar(L"PK"), &gEfiGlobalVariableGuid)) {
         setup_mode = SETUP_MODE;
         deployed_mode = 0;
     } else {
         secure_boot = secure_boot_enabled;
     }
 
-    status = storage_set(L"SetupMode", sizeof_wchar(L"SetupMode"), &gEfiGlobalVariableGuid, &setup_mode,
-                         sizeof(setup_mode), EFI_VARIABLE_BOOTSERVICE_ACCESS |
-                         EFI_VARIABLE_RUNTIME_ACCESS);
+    status = storage_set(
+            L"SetupMode", sizeof_wchar(L"SetupMode"), &gEfiGlobalVariableGuid,
+            &setup_mode, sizeof(setup_mode),
+            EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS);
 
     if (status != EFI_SUCCESS)
         return status;
 
-    status = storage_set(L"SignatureSupport", sizeof_wchar(L"SignatureSupport"), &gEfiGlobalVariableGuid,
-                         signature_support, sizeof(signature_support),
+    status = storage_set(L"SignatureSupport", sizeof_wchar(L"SignatureSupport"),
+                         &gEfiGlobalVariableGuid, signature_support,
+                         sizeof(signature_support),
                          EFI_VARIABLE_BOOTSERVICE_ACCESS |
                                  EFI_VARIABLE_RUNTIME_ACCESS);
 
     if (status != EFI_SUCCESS)
         return status;
 
-    status = storage_set(L"SecureBoot", sizeof_wchar(L"SecureBoot"),
-                         &gEfiGlobalVariableGuid, &secure_boot,
-                         sizeof(secure_boot), EFI_VARIABLE_BOOTSERVICE_ACCESS |
-                         EFI_VARIABLE_RUNTIME_ACCESS);
+    status = storage_set(
+            L"SecureBoot", sizeof_wchar(L"SecureBoot"), &gEfiGlobalVariableGuid,
+            &secure_boot, sizeof(secure_boot),
+            EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS);
 
     if (status != EFI_SUCCESS)
         return status;
 
-    status = storage_set(L"AuditMode", sizeof_wchar(L"AuditMode"),
-                         &gEfiGlobalVariableGuid, &audit_mode,
-                         sizeof(audit_mode), EFI_VARIABLE_BOOTSERVICE_ACCESS |
-                         EFI_VARIABLE_RUNTIME_ACCESS);
+    status = storage_set(
+            L"AuditMode", sizeof_wchar(L"AuditMode"), &gEfiGlobalVariableGuid,
+            &audit_mode, sizeof(audit_mode),
+            EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS);
 
     if (status != EFI_SUCCESS)
         return status;
 
-    status = storage_set(L"DeployedMode", sizeof_wchar(L"DeployedMode"),
-                         &gEfiGlobalVariableGuid, &deployed_mode,
-                         sizeof(deployed_mode), EFI_VARIABLE_BOOTSERVICE_ACCESS |
-                         EFI_VARIABLE_RUNTIME_ACCESS);
+    status = storage_set(
+            L"DeployedMode", sizeof_wchar(L"DeployedMode"),
+            &gEfiGlobalVariableGuid, &deployed_mode, sizeof(deployed_mode),
+            EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS);
 
     if (status != EFI_SUCCESS)
         return status;
@@ -215,28 +213,34 @@ auth_lib_initialize(struct auth_data *auths, size_t n)
  * @return EFI_SUCCESS if variable is successfully stored, otherwise an EFI error code
  */
 EFI_STATUS
-auth_lib_process_variable(UTF16 *variable_name, size_t namesz, EFI_GUID *vendor_guid,
-                               void *data, uint64_t data_size,
-                               uint32_t attributes)
+auth_lib_process_variable(UTF16 *variable_name, size_t namesz,
+                          EFI_GUID *vendor_guid, void *data, uint64_t data_size,
+                          uint32_t attributes)
 {
-  EFI_STATUS status;
+    EFI_STATUS status;
 
-  if (compare_guid(vendor_guid, &gEfiGlobalVariableGuid) && (strcmp16(variable_name, EFI_PLATFORM_KEY_NAME) == 0)){
-    status = process_var_with_pk(variable_name, vendor_guid, data, data_size, attributes, true);
-  } else if (compare_guid(vendor_guid, &gEfiGlobalVariableGuid) && (strcmp16(variable_name, EFI_KEY_EXCHANGE_KEY_NAME) == 0)) {
-    status = process_var_with_pk(variable_name, vendor_guid, data, data_size, attributes, false);
-  } else if (compare_guid(vendor_guid, &gEfiImageSecurityDatabaseGuid) &&
-             ((strcmp16(variable_name, EFI_IMAGE_SECURITY_DATABASE)  == 0) ||
-              (strcmp16(variable_name, EFI_IMAGE_SECURITY_DATABASE1) == 0) ||
-              (strcmp16(variable_name, EFI_IMAGE_SECURITY_DATABASE2) == 0))) {
-
-        status = process_var_with_pk(variable_name, vendor_guid, data, data_size, attributes, false);
+    if (compare_guid(vendor_guid, &gEfiGlobalVariableGuid) &&
+        (strcmp16(variable_name, EFI_PLATFORM_KEY_NAME) == 0)) {
+        status = process_var_with_pk(variable_name, vendor_guid, data,
+                                     data_size, attributes, true);
+    } else if (compare_guid(vendor_guid, &gEfiGlobalVariableGuid) &&
+               (strcmp16(variable_name, EFI_KEY_EXCHANGE_KEY_NAME) == 0)) {
+        status = process_var_with_pk(variable_name, vendor_guid, data,
+                                     data_size, attributes, false);
+    } else if (compare_guid(vendor_guid, &gEfiImageSecurityDatabaseGuid) &&
+               ((strcmp16(variable_name, EFI_IMAGE_SECURITY_DATABASE) == 0) ||
+                (strcmp16(variable_name, EFI_IMAGE_SECURITY_DATABASE1) == 0) ||
+                (strcmp16(variable_name, EFI_IMAGE_SECURITY_DATABASE2) == 0))) {
+        status = process_var_with_pk(variable_name, vendor_guid, data,
+                                     data_size, attributes, false);
         if (status != EFI_SUCCESS) {
-            status = process_var_with_kek(variable_name, vendor_guid, data, data_size, attributes);
+            status = process_var_with_kek(variable_name, vendor_guid, data,
+                                          data_size, attributes);
         }
-  } else {
-    status = process_variable(variable_name, vendor_guid, data, data_size, attributes);
-  }
+    } else {
+        status = process_variable(variable_name, vendor_guid, data, data_size,
+                                  attributes);
+    }
 
-  return status;
+    return status;
 }
