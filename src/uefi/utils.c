e4a6c9cdb6af28014fa62d5d37b79bff55067334
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-11-20 11:29:44 -0800

    refactor(utils.c): fix style

--- src/uefi/utils.c
+++ src/uefi/utils.c
@@ -11,14 +11,19 @@ bool is_secure_boot_variable(UTF16 *name, size_t namesz, EFI_GUID *guid)
         return false;
 
     if (compare_guid(guid, &gEfiGlobalVariableGuid)) {
-        return ((namesz == sizeof_wchar(L"PK") && memcmp(name, L"PK", namesz) == 0) ||
-                (namesz == sizeof_wchar(L"KEK") && memcmp(name, L"KEK", namesz) == 0));
+        return ((namesz == sizeof_wchar(L"PK") &&
+                 memcmp(name, L"PK", namesz) == 0) ||
+                (namesz == sizeof_wchar(L"KEK") &&
+                 memcmp(name, L"KEK", namesz) == 0));
     }
 
     if (compare_guid(guid, &gEfiImageSecurityDatabaseGuid)) {
-        return ((namesz == sizeof_wchar(L"db") && memcmp(name, L"db", namesz) == 0) ||
-                (namesz == sizeof_wchar(L"dbx") && memcmp(name, L"dbx", namesz) == 0) ||
-                (namesz == sizeof_wchar(L"dbt") && memcmp(name, L"dbt", namesz) == 0));
+        return ((namesz == sizeof_wchar(L"db") &&
+                 memcmp(name, L"db", namesz) == 0) ||
+                (namesz == sizeof_wchar(L"dbx") &&
+                 memcmp(name, L"dbx", namesz) == 0) ||
+                (namesz == sizeof_wchar(L"dbt") &&
+                 memcmp(name, L"dbt", namesz) == 0));
     }
 
     return false;
