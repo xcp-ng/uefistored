3722f56bd7935a2a9475971c47740931d8aafb09
Bobby Eshleman <bobbyeshleman@gmail.com>
2020-12-04 15:49:01 -0800

    fix(deploy.sh): do not send PK.auth

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,4 +10,3 @@ fi
 UEFISTORE=/usr/sbin/uefistored
 scp -P${PORT} uefistored root@${HOST}:${UEFISTORE}
 ssh -p${PORT} root@${HOST} -- ln -sf ${UEFISTORE} /usr/sbin/varstored
-scp -P${PORT} scripts/certs/PK.auth root@${HOST}:/usr/share/varstored/PK.auth
