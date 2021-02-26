1167b22408684f38da4937446d5f2729c2b5e7bb
Bobby Eshleman <bobby.eshleman@gmail.com>
2021-02-25 16:50:41 -0800

    tests: remove unused inc/test_*
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>

--- tests/inc/test_auth.h
+++
@@ -1,6 +0,0 @@
-#ifndef __H_TEST_AUTH_
-#define __H_TEST_AUTH_
-
-void test_auth(void);
-
-#endif // __H_TEST_AUTH_
--- tests/inc/test_base64.h
+++
@@ -1,6 +0,0 @@
-#ifndef __H_TEST_BASE64_
-#define __H_TEST_BASE64_
-
-int test_base64(void);
-
-#endif //  __H_TEST_BASE64_
