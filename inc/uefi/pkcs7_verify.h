d1a6805a39cd305d2d807289867fc01300683b58
Bobby Eshleman <bobby.eshleman@gmail.com>
2021-02-25 17:38:13 -0800

    auth: remove old Pkcs7Verify
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>

--- inc/uefi/pkcs7_verify.h
+++ inc/uefi/pkcs7_verify.h
@@ -4,25 +4,6 @@
 #include "uefi/image_authentication.h"
 #include <openssl/x509.h>
 
-bool Pkcs7GetSigners (
-  const uint8_t   *P7Data,
-  uint64_t         P7Length,
-  STACK_OF(X509) **CertStack,
-  uint64_t        *StackLength
-  );
-
-bool Pkcs7Verify (
-  const uint8_t  *P7Data,
-  uint64_t        P7Length,
-  const X509  *TrustedCert,
-  const uint8_t  *InData,
-  uint64_t        DataLength
-  );
-
-void Pkcs7FreeSigners (
-  X509        *Certs
-  );
-
 uint8_t *X509_to_buf(X509 *cert, int *len);
 
 static inline X509* X509_from_buf(const uint8_t *buf, long len)
