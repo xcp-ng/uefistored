de708083b345671d47a66f02540020037a1b1f6b
Bobby Eshleman <bobby.eshleman@gmail.com>
2021-05-06 17:38:46 -0700

    scripts: update create-certs to make db2
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>

--- scripts/certs/create-certs.sh
+++ scripts/certs/create-certs.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# This script generates a set of PK variables that can be used to test PK functionality.
-# The output certs are numbered in the order in which they should be set to test
-# various PK variable functionalities.  They should be set using the command:
+# This script generates a set of auth variables that can be used to test secure boot.
+# They should be set using the command:
 #
 # $ efi-updatevar -f <auth_file> PK
 
@@ -10,34 +9,31 @@ set -e
 
 GUID="e8e1f046-04f5-4e5c-ba83-5a78e828cd2d"
 
+owner=$RANDOM
+
 create_x509() {
     local var=$1
 
     openssl req \
         -newkey rsa:4096 -nodes -keyout ${var}.key \
         -new -x509 -sha256 -days 3650   \
-        -subj "/CN=${var} Owner/" -out ${var}.crt
+        -subj "/CN=${owner} Owner/" -out ${var}.crt
 
     openssl x509 -outform DER -in ${var}.crt -out ${var}.der
 }
 
 create_esl() {
-    local var=$1
+    local crt=$1
+    local esl=$2
 
-    cert-to-efi-sig-list -g "${GUID}" ${var}.crt ${var}.esl
+    cert-to-efi-sig-list -g "${GUID}" ${crt} ${esl}
 }
 
 sign() {
     local signer=$1
     local var=$2
-    local auth=$3
-
-    if [[ "$@" =~ "--null" ]];
-    then
-        esl=null.esl
-    else
-        esl=${var}.esl
-    fi
+    local esl=$3
+    local auth=$4
 
     # Wait a second to ensure a new timestamp
     sleep 1
@@ -50,14 +46,18 @@ set -x
 
 > null.esl
 
-create_x509 PK
 create_x509 KEK
-create_x509 db
 create_x509 newPK
 
-create_esl PK
-create_esl KEK
-create_esl db
+create_x509 PK
+create_esl PK.crt PK.esl
+create_esl KEK.crt KEK.esl
+
+create_x509 db
+create_esl db.crt db.esl
+
+create_x509 db2
+create_esl db2.crt db2.esl
 
 sleep 1
 sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %H:%M:%S')" -g "${GUID}" \
@@ -67,27 +67,32 @@ sleep 1
 sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %H:%M:%S')"  -g "${GUID}" \
                   -k KEK.key -c KEK.crt db db.esl db-signed-by-KEK.auth
 
-sign PK PK PK.auth
-sign PK KEK KEK.auth
+sign PK PK PK.esl PK.auth
+sign PK KEK KEK.esl KEK.auth
 
-sign PK PK nullPK.auth --null
-sign PK KEK nullKEK.auth --null
-sign KEK db null-db-signed-by-KEK.auth --null
-sign PK db null-db-signed-by-PK.auth --null
+sign PK PK null.esl nullPK.auth
+sign PK KEK null.esl nullKEK.auth
+sign KEK db null.esl null-db-signed-by-KEK.auth
+sign PK db null.esl null-db-signed-by-PK.auth
 
-sign PK db db.auth
+sign KEK db db.esl db.auth
+
+sleep 2
+
+sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %T')" -g "${GUID}" \
+                  -k KEK.key -c KEK.crt db db2.esl db2.auth
 
 sleep 1
 
 # A new and good PK
-create_esl newPK
+create_esl newPK.crt newPK.esl
 sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %T')" -g "${GUID}" \
                   -k PK.key -c PK.crt PK newPK.esl newPK.auth
 
 # Bad PK
-create_x509 PK
-create_esl badPK
-sign PK badPK badPK.auth
+create_x509 badPK
+create_esl badPK.crt badPK.esl
+sign PK badPK badPK.esl badPK.auth
 
 mkdir -p test-certs/
 mv *.der *.esl *.key *.crt *.auth test-certs/
