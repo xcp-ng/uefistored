3b40c5e54b203feb065d4fb996782bfbab65a8e2
Bobby Eshleman <bobby.eshleman@gmail.com>
2021-08-11 12:23:20 -0700

    test: update certificates
    Simplify create-certs.sh and create new certificates for testing
    (importantly, add a new/correct db-append).
    
    Signed-off-by: Bobby Eshleman <bobby.eshleman@gmail.com>

--- scripts/certs/create-certs.sh
+++ scripts/certs/create-certs.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # This script generates a set of auth variables that can be used to test secure boot.
-# They should be set using the command:
-#
-# $ efi-updatevar -f <auth_file> PK
 
 set -e
 
-GUID="e8e1f046-04f5-4e5c-ba83-5a78e828cd2d"
-
-owner=$RANDOM
+GLOBAL_GUID="8be4df61-93ca-11d2-aa0d-00e098032b8c"
+DB_GUID="d719b2cb-3d3a-4596-a3bc-dad00e67656f"
+seconds=0
 
 create_x509() {
     local var=$1
@@ -17,82 +14,72 @@ create_x509() {
     openssl req \
         -newkey rsa:4096 -nodes -keyout ${var}.key \
         -new -x509 -sha256 -days 3650   \
-        -subj "/CN=${owner} Owner/" -out ${var}.crt
+        -subj "/CN=Test Owner/" -out ${var}.crt
 
     openssl x509 -outform DER -in ${var}.crt -out ${var}.der
 }
 
-create_esl() {
-    local crt=$1
-    local esl=$2
-
-    cert-to-efi-sig-list -g "${GUID}" ${crt} ${esl}
+new_time() {
+    date --date="${1} second" +'%Y-%m-%d %T'
 }
-
-sign() {
-    local signer=$1
-    local var=$2
-    local esl=$3
-    local auth=$4
-
-    # Wait a second to ensure a new timestamp
-    sleep 1
-
-    sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %T')" -g "${GUID}" \
-                      -k ${signer}.key -c ${signer}.crt ${var} ${esl} ${auth}
-}
-
-set -x
 
 > null.esl
 
-create_x509 KEK
-create_x509 newPK
-
 create_x509 PK
-create_esl PK.crt PK.esl
-create_esl KEK.crt KEK.esl
-
+create_x509 newPK
+create_x509 badPK
+create_x509 KEK
 create_x509 db
-create_esl db.crt db.esl
-
+create_x509 db-append
 create_x509 db2
-create_esl db2.crt db2.esl
 
-sleep 1
-sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %H:%M:%S')" -g "${GUID}" \
+cert-to-efi-sig-list -g "${GLOBAL_GUID}" PK.crt PK.esl
+cert-to-efi-sig-list -g "${GLOBAL_GUID}" KEK.crt KEK.esl
+cert-to-efi-sig-list -g "${DB_GUID}" db.crt db.esl
+cert-to-efi-sig-list -g "${DB_GUID}" db-append.crt db-append.esl
+cert-to-efi-sig-list -g "${DB_GUID}" db2.crt db2.esl
+cert-to-efi-sig-list -g "${DB_GUID}" newPK.crt newPK.esl
+cert-to-efi-sig-list -g "${DB_GUID}" badPK.crt badPK.esl
+
+set -x
+
+# dbs
+let "seconds=seconds+1"
+sign-efi-sig-list -t "$(new_time ${seconds})" -g "${DB_GUID}" \
+                  -k KEK.key -c KEK.crt db db.esl db.auth
+let "seconds=seconds+1"
+sign-efi-sig-list -t "$(new_time ${seconds})" -g "${DB_GUID}" \
                   -k PK.key -c PK.crt db db.esl db-signed-by-PK.auth
-
-sleep 1
-sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %H:%M:%S')"  -g "${GUID}" \
+let "seconds=seconds+1"
+sign-efi-sig-list -t "$(new_time ${seconds})"  -g "${DB_GUID}" \
                   -k KEK.key -c KEK.crt db db.esl db-signed-by-KEK.auth
 
-sign PK PK PK.esl PK.auth
-sign PK KEK KEK.esl KEK.auth
+# db with append attribute
+let "seconds=seconds+1"
+sign-efi-sig-list -a -t "$(new_time ${seconds})" -g "${DB_GUID}" \
+                  -k KEK.key -c KEK.crt db db-append.esl db-append.auth
 
-sign PK PK null.esl nullPK.auth
-sign PK KEK null.esl nullKEK.auth
-sign KEK db null.esl null-db-signed-by-KEK.auth
-sign PK db null.esl null-db-signed-by-PK.auth
-
-sign KEK db db.esl db.auth
-
-sleep 2
-
-sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %T')" -g "${GUID}" \
-                  -k KEK.key -c KEK.crt db db2.esl db2.auth
-
-sleep 1
-
-# A new and good PK
-create_esl newPK.crt newPK.esl
-sign-efi-sig-list -t "$(date --date='1 second' +'%Y-%m-%d %T')" -g "${GUID}" \
+# PKs
+let "seconds=seconds+1"
+sign-efi-sig-list -t "$(new_time ${seconds})"  -g "${GLOBAL_GUID}" \
+                  -k PK.key -c PK.crt PK PK.esl PK.auth
+let "seconds=seconds+1"
+sign-efi-sig-list -t "$(new_time ${seconds})" -g "${GLOBAL_GUID}" \
                   -k PK.key -c PK.crt PK newPK.esl newPK.auth
+let "seconds=seconds+1"
+sign-efi-sig-list -t "$(new_time ${seconds})" -g "${GLOBAL_GUID}" \
+                  -k PK.key -c PK.crt PK null.esl nullPK.auth
+let "seconds=seconds+1"
+sign-efi-sig-list -t "$(new_time ${seconds})" -g "${GLOBAL_GUID}" \
+                  -k badPK.key -c badPK.crt PK badPK.esl badPK.auth
 
-# Bad PK
-create_x509 badPK
-create_esl badPK.crt badPK.esl
-sign PK badPK badPK.esl badPK.auth
+# KEKs
+let "seconds=seconds+1"
+sign-efi-sig-list -t "$(new_time ${seconds})" -g "${GLOBAL_GUID}" \
+                  -k PK.key -c PK.crt KEK KEK.esl KEK.auth
+
+set +x
 
 mkdir -p test-certs/
+yes | rm test-certs/*
 mv *.der *.esl *.key *.crt *.auth test-certs/
